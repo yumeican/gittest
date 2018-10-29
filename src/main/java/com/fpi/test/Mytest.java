763b5a876647f8221d8095d4d4433ff75043211d
yumeican <2350840199@qq.com>
2018-10-29 13:56:03 +0800

    yic

---
+++ src/main/java/com/fpi/test/Mytest.java
@@ -0,0 +1,10 @@
+package com.fpi.test;
+
+public class Mytest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("开始git测试");
+	}
+
+}
---
+++ src/main/java/com/fpi/test/mytest1.java
@@ -0,0 +1,5 @@
+package com.fpi.test;
+
+public class mytest1 {
+
+}
